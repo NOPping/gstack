0be4d76408d2cd015f2eab5047765ef8f0356f14
Darren Brogan <darrenbrogan@hotmail.com>
2013-09-10 17:33:18 -0400

    Apply pep8 formatting to index.py

--- gcecloudstack/controllers/index.py
+++ gcecloudstack/controllers/index.py
@@ -25,65 +25,76 @@ from flask import jsonify
 import json
 import os
 
+
 @app.route('/example')
 @authentication.required
 def example(authorization):
     resp = jsonify({
-            "jsessionid": authorization.jsessionid,
-            "sessionkey": authorization.sessionkey,
-        })
+        "jsessionid": authorization.jsessionid,
+        "sessionkey": authorization.sessionkey,
+    })
     resp.status_code = 200
     return resp
 
+
 @app.route('/compute/v1beta15/projects/<projectid>/zones')
 @authentication.required
-def listzones(projectid,authorization):
-    
+def listzones(projectid, authorization):
+
     command = 'listZones'
     args = {}
     logger = None
-    cloudstack_response = requester.make_request(command, args, logger, authorization.jsessionid, authorization.sessionkey)
+    cloudstack_response = requester.make_request(
+        command,
+        args,
+        logger,
+        authorization.jsessionid,
+        authorization.sessionkey
+    )
     print(cloudstack_response)
     cloudstack_response = json.loads(cloudstack_response)
     num_zones = cloudstack_response['listzonesresponse']['count']
     num_zones = int(num_zones)
     cloudstack_response = cloudstack_response['listzonesresponse']['zone']
 
+    zones = []
 
-    zones = [] 
-    
     current_zone = 0
     while current_zone < num_zones:
         for item in cloudstack_response:
-		    zones.append({
-		        'kind' : "compute#zone",
-		        'name': item['name'],
-		        'instances': '',
-			    'description': item['name'],
-			    'id' : item['id'],
-			    'status' : item['allocationstate']
+            zones.append({
+                'kind': "compute#zone",
+                'name': item['name'],
+                'description': item['name'],
+                'id': item['id'],
+                'status': item['allocationstate']
             })
         current_zone += 1
- 
-   
+
     populated_response = {
-	   'kind' : "compute#zoneList",
-	   'id' : '',
-	   'selfLink': '',
-	   'items' : zones
+        'kind': "compute#zoneList",
+        'id': '',
+        'selfLink': '',
+        'items': zones
     }
 
-    
     gcutil_responce = jsonify(populated_response)
     gcutil_responce.status_code = 200
     return gcutil_responce
 
+
 @app.route('/compute/v1beta15/projects/<projectid>')
 @authentication.required
-def getProject(projectid,authorization):
+def getProject(projectid, authorization):
 
     basepath = os.path.dirname(__file__)
-    getproject_template = open(os.path.join(basepath,"..", "templates/getproject.json"), "r")
+    getproject_template = open(
+        os.path.join(
+            basepath,
+            "..",
+            "templates/getproject.json"
+         ),"r"
+    )
     getproject_template = json.loads(getproject_template.read())
     resp = jsonify(getproject_template)
 
@@ -92,11 +103,18 @@ def getProject(projectid,authorization):
 
     return resp
 
+
 @app.route('/discovery/v1/apis/compute/v1beta15/rest')
 def discovery():
 
     basepath = os.path.dirname(__file__)
-    discovery_template = open(os.path.join(basepath,"..", "templates/discovery.json"), "r")
+    discovery_template = open(
+        os.path.join(
+            basepath,
+            "..",
+            "templates/discovery.json"
+        ),"r"
+    )
     discovery_template = json.loads(discovery_template.read())
     resp = jsonify(discovery_template)
 
