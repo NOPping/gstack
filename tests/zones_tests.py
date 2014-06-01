4c8c45c8f7011c6698842a19db831ce6e4e7a11f
BroganD1993 <darrenbrogan@hotmail.com>
2014-06-01 15:06:16 +0100

    Extennd on Zone tests

--- tests/zones_tests.py
+++ tests/zones_tests.py
@@ -13,12 +13,39 @@ class ZonesTestCase(GStackAppTestCase):
     def test_list_zones(self):
 
         get = mock.Mock()
-        get.return_value.text = read_file('tests/data/zones_search.json')
+        get.return_value.text = read_file('tests/data/valid_describe_zone.json')
         get.return_value.status_code = 200
 
         with mock.patch('requests.get', get):
             headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
-            print headers
             response = self.get('/compute/v1/projects/exampleproject/zones', headers=headers)
 
         self.assert_ok(response)
+
+    def test_get_zone(self):
+
+        get = mock.Mock()
+        get.return_value.text = read_file('tests/data/valid_describe_zone.json')
+        get.return_value.status_code = 200
+
+        with mock.patch('requests.get', get):
+            headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
+            response = self.get('/compute/v1/projects/exampleproject/zones/zonename', headers=headers)
+
+        self.assert_ok(response)
+
+    def test_get_zone_not_found(self):
+
+        get = mock.Mock()
+        get.return_value.text = read_file('tests/data/empty_describe_zone.json')
+        get.return_value.status_code = 200
+
+        with mock.patch('requests.get', get):
+            headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
+            response = self.get('/compute/v1/projects/exampleproject/zones/zonename', headers=headers)
+
+        print response.data
+
+        self.assert_not_found(response)
+        assert 'The resource \'/compute/v1/projects/exampleproject/zones/zonename\' was not found' \
+                in response.data
