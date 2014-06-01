4f6c65098ae3465331197c173b4924f48e68f982
BroganD1993 <darrenbrogan@hotmail.com>
2014-06-01 15:26:01 +0100

    Add image unittests

--- tests/images_tests.py
+++ tests/images_tests.py
@@ -8,40 +8,40 @@ from . import GStackAppTestCase
 
 class ZonesTestCase(GStackAppTestCase):
 
-    def test_list_zones(self):
+    def test_list_images(self):
 
         get = mock.Mock()
-        get.return_value.text = read_file('tests/data/valid_describe_zone.json')
+        get.return_value.text = read_file('tests/data/valid_describe_images.json')
         get.return_value.status_code = 200
 
         with mock.patch('requests.get', get):
             headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
-            response = self.get('/compute/v1/projects/exampleproject/zones', headers=headers)
+            response = self.get('/compute/v1/projects/exampleproject/global/images', headers=headers)
 
         self.assert_ok(response)
 
-    def test_get_zone(self):
+    def test_get_image(self):
 
         get = mock.Mock()
-        get.return_value.text = read_file('tests/data/valid_describe_zone.json')
+        get.return_value.text = read_file('tests/data/valid_describe_images.json')
         get.return_value.status_code = 200
 
         with mock.patch('requests.get', get):
             headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
-            response = self.get('/compute/v1/projects/exampleproject/zones/zonename', headers=headers)
+            response = self.get('/compute/v1/projects/exampleproject/global/images/imagename', headers=headers)
 
         self.assert_ok(response)
 
     def test_get_zone_not_found(self):
 
         get = mock.Mock()
-        get.return_value.text = read_file('tests/data/empty_describe_zone.json')
+        get.return_value.text = read_file('tests/data/empty_describe_images.json')
         get.return_value.status_code = 200
 
         with mock.patch('requests.get', get):
             headers = {'authorization': 'Bearer ' + str(GStackAppTestCase.access_token)}
-            response = self.get('/compute/v1/projects/exampleproject/zones/zonename', headers=headers)
+            response = self.get('/compute/v1/projects/exampleproject/global/images/imagename', headers=headers)
 
         self.assert_not_found(response)
-        assert 'The resource \'/compute/v1/projects/exampleproject/zones/zonename\' was not found' \
+        assert 'The resource \'/compute/v1/projects/exampleproject/global/images/imagename\' was not found' \
                 in response.data
--- tests/sample_tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from . import GStackAppTestCase
-
-class SampleTestCase(GStackAppTestCase):
-
-    def test_sample(self):
-        pass
